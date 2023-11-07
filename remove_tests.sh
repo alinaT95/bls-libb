e9e465049bf18ee9c57ab92332875b09eaaefb6d
alinaT95 <alina.t@tonlabs.io>
2023-11-08 00:35:56 +0300

    remove file

--- remove_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-find . -name tests -type d -print0 | xargs -0 rm -rf
-find ./ -name Cargo.toml -type f -print0 | xargs -0 sed -i -E "s/git = (\"|')(ssh|https):\/\/(git@)?github.com\/tonlabs\/ever-([A-Za-z0-9_-]*)-private(\.git)?/git =\1https:\/\/github.com\/tonlabs\/ever-\4\5/g"
-
-rm -f *.sh
